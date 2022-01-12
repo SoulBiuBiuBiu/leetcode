98a338e4dc1f25a7351d764d70e117f04828ef20
Barry <1172807506@qq.com>
2022-01-12 14:35:45 +0800

    'P021'

---
+++ src/main/java/org/barry/algorithm/offer2/P021.java
@@ -0,0 +1,25 @@
+package org.barry.algorithm.offer2;
+
+import org.barry.algorithm.ListNode;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/1/12 14:26
+ */
+public class P021 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head), slow = dummy, fast = head;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
