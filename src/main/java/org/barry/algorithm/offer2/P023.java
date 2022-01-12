1abe89011a842a88c3e31d7ebada26b58543e6bd
Barry <1172807506@qq.com>
2022-01-12 15:05:45 +0800

    'P023'

---
+++ src/main/java/org/barry/algorithm/offer2/P023.java
@@ -0,0 +1,24 @@
+package org.barry.algorithm.offer2;
+
+import org.barry.algorithm.ListNode;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/1/12 14:53
+ */
+public class P023 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
