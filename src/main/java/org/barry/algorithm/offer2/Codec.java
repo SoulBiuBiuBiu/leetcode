8de0e915151fb8254b55d17c94ecc0d88a9f4aa6
Barry <1172807506@qq.com>
2022-01-26 15:44:22 +0800

    'P048'

---
+++ src/main/java/org/barry/algorithm/offer2/Codec.java
@@ -0,0 +1,56 @@
+package org.barry.algorithm.offer2;
+
+import org.barry.algorithm.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+        StringBuilder res = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if(node != null){
+                res.append("" + node.val);
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }else{
+                res.append("null");
+            }
+            res.append(",");
+        }
+        return res.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data.equals("")){
+            return null;
+        }
+        String[] dataList = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(dataList[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int i = 1;
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if(!"null".equals(dataList[i])){
+                node.left = new TreeNode(Integer.parseInt(dataList[i]));
+                queue.offer(node.left);
+            }
+            i++;
+            if(!"null".equals(dataList[i])){
+                node.right = new TreeNode(Integer.parseInt(dataList[i]));
+                queue.offer(node.right);
+            }
+            i++;
+        }
+        return root;
+    }
+}
