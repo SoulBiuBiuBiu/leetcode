000d3a0655f3eb3181eaac6a401e71bfbbbf8caf
Barry <1172807506@qq.com>
2022-02-11 14:35:30 +0800

    'Hot100 P048'

---
+++ src/main/java/org/barry/algorithm/hot100/P048.java
@@ -0,0 +1,31 @@
+package org.barry.algorithm.hot100;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/2/10 16:06
+ */
+public class P048 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+        // 主对角线翻转
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+}
