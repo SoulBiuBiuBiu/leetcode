776cc44e4ddb8420853d935b35e35d8c65a4ae10
Barry <1172807506@qq.com>
2022-02-17 17:06:17 +0800

    'Hot100 P114'

---
+++ src/main/java/org/barry/algorithm/P114.java
@@ -0,0 +1,27 @@
+package org.barry.algorithm;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/2/17 16:48
+ */
+public class P114 {
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode next = curr.left;
+                TreeNode predecessor = next;
+                while (predecessor.right != null) {
+                    predecessor = predecessor.right;
+                }
+                predecessor.right = curr.right;
+                curr.left = null;
+                curr.right = next;
+            }
+            curr = curr.right;
+        }
+    }
+}
