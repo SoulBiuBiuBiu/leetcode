63254c3acec6db24f86fe51b097e599c76ab564a
Barry <1172807506@qq.com>
2022-02-24 20:09:23 +0800

    'Hot100 P148'

---
+++ src/main/java/org/barry/algorithm/hot100/P148.java
@@ -0,0 +1,61 @@
+package org.barry.algorithm.hot100;
+
+import org.barry.algorithm.ListNode;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/2/24 19:50
+ */
+public class P148 {
+    public ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
+
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
+            return head;
+        }
+
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        return merge(list1, list2);
+    }
+
+    private ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0), cur1 = list1, cur2 = list2, cur = dummy;
+
+        while (cur1 != null && cur2 != null) {
+            if (cur1.val <= cur2.val) {
+                cur.next = cur1;
+                cur1 = cur1.next;
+            } else {
+                cur.next = cur2;
+                cur2 = cur2.next;
+            }
+            cur = cur.next;
+        }
+        if (cur1 != null) {
+            cur.next = cur1;
+        } else if (cur2 != null) {
+            cur.next = cur2;
+        }
+        return dummy.next;
+    }
+}
