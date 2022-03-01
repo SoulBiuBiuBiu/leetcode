5684f22fcb9ddfe5747ded5aecf10c5d32f8a26a
Barry <1172807506@qq.com>
2022-03-01 20:00:34 +0800

    'Hot100 P279'

---
+++ src/main/java/org/barry/algorithm/hot100/P279.java
@@ -0,0 +1,24 @@
+package org.barry.algorithm.hot100;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/3/1 19:38
+ */
+public class P279 {
+    public int numSquares(int n) {
+        //dp[i]
+        //dp[i]=Math.min(dp[i-j], dp[j]);
+        int[] f = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, f[i - j * j]);
+            }
+            f[i] = min + 1;
+        }
+        return f[n];
+    }
+}
