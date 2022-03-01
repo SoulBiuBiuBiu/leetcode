fb02cd9777bf717e4294e183109b81b71052c279
Barry <1172807506@qq.com>
2022-03-01 20:11:56 +0800

    'Hot100 P287'

---
+++ src/main/java/org/barry/algorithm/hot100/P287.java
@@ -0,0 +1,27 @@
+package org.barry.algorithm.hot100;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/3/1 20:00
+ */
+public class P287 {
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        int pre1 = 0;
+        int pre2 = slow;
+        while (pre1 != pre2) {
+            pre1 = nums[pre1];
+            pre2 = nums[pre2];
+        }
+        return pre1;
+    }
+}
