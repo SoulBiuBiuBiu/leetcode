ff73be731e2575ec31e4bd844df8b362158ac55a
Barry <1172807506@qq.com>
2022-03-01 17:43:44 +0800

    'Hot100 P236'

---
+++ src/main/java/org/barry/algorithm/hot100/P236.java
@@ -0,0 +1,23 @@
+package org.barry.algorithm.hot100;
+
+import org.barry.algorithm.TreeNode;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/3/1 17:24
+ */
+public class P236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) return right;
+        if (right == null) return left;
+        return root;
+    }
+}
