6f7a922661965204bb01acec59823931dd3b6d4c
Barry <1172807506@qq.com>
2022-03-08 11:06:08 +0800

    'Hot100 P337'

---
+++ src/main/java/org/barry/algorithm/hot100/P337.java
@@ -0,0 +1,28 @@
+package org.barry.algorithm.hot100;
+
+import org.barry.algorithm.TreeNode;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/3/8 10:37
+ */
+public class P337 {
+    public int rob(TreeNode root) {
+        int[] rootStatus = dfs(root);
+        return Math.max(rootStatus[0], rootStatus[1]);
+    }
+
+    public int[] dfs(TreeNode node) {
+        if (node == null) {
+            return new int[2];
+        }
+        int[] l = dfs(node.left);
+        int[] r = dfs(node.right);
+        int selected = node.val + l[1] + r[1];
+        int notSelected = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+        return new int[]{selected, notSelected};
+    }
+}
