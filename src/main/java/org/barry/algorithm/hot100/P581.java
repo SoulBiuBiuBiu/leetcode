3e3d7ab14e36e04a439cf3d1cc2e7f88284d4c24
Barry <1172807506@qq.com>
2022-03-16 10:16:27 +0800

    'Hot100 P581'

---
+++ src/main/java/org/barry/algorithm/hot100/P581.java
@@ -0,0 +1,22 @@
+package org.barry.algorithm.hot100;
+
+import java.util.Arrays;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/3/16 9:59
+ */
+public class P581 {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int[] arr = nums.clone();
+        Arrays.sort(arr);
+        int i = 0, j = n - 1;
+        while (i <= j && nums[i] == arr[i]) i++;
+        while (i <= j && nums[j] == arr[j]) j--;
+        return j - i + 1;
+    }
+}
