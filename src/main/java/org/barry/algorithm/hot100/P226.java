a68213fcec5004baac5efad6c1d59e48aa22fcb9
Barry <1172807506@qq.com>
2022-04-06 17:29:57 +0800

    'Hot100 P226'

---
+++ src/main/java/org/barry/algorithm/hot100/P226.java
@@ -0,0 +1,24 @@
+package org.barry.algorithm.hot100;
+
+import org.barry.algorithm.TreeNode;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/4/6 17:23
+ */
+public class P226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
