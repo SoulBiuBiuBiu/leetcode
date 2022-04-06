6e344f7b72a372e693fb15c9c3356305882569f4
Barry <1172807506@qq.com>
2022-04-06 17:22:34 +0800

    'Hot100 P221-2'

--- src/main/java/org/barry/algorithm/hot100/P221.java
+++ src/main/java/org/barry/algorithm/hot100/P221.java
@@ -9,20 +9,19 @@ package org.barry.algorithm.hot100;
  */
 public class P221 {
     public int maximalSquare(char[][] matrix) {
-        int height = matrix.length, width = matrix[0].length;
+        //dp[i][j]表示以matrix[i][j]为右下角的最大正方形边长
+        int[][] dp = new int[matrix.length][matrix[0].length];
         int max = 0;
-        int[][] dp = new int[height][width];
-
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == '1') {
                     if (i == 0 || j == 0) {
-                        dp[i][j]=1;
-                    }else{
-                        dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
                     }
+                    max = Math.max(max, dp[i][j]);
                 }
-                max = Math.max(max, dp[i][j]);
             }
         }
         return max * max;
