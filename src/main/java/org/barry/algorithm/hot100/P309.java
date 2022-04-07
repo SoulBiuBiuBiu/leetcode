1c708de96a70dc67f14863199b0e632a6e6a441a
Barry <1172807506@qq.com>
2022-04-07 10:48:33 +0800

    'Hot100 P309-2'

--- src/main/java/org/barry/algorithm/hot100/P309.java
+++ src/main/java/org/barry/algorithm/hot100/P309.java
@@ -10,28 +10,23 @@ package org.barry.algorithm.hot100;
 public class P309 {
     public int maxProfit(int[] prices) {
         //dp[i][0] 持有
-        // 持有             dp[i][0]=Math.max(dp[i-1][2]-prices[i],dp[i-1][0])
-        // 不持有 冷冻       dp[i][1]=dp[i-1][0]+prices[i]
-        // 不持有 不冷冻     dp[i][2]=Math.max(dp[i-1][1],dp[i-1][2])
-
-        if (prices.length == 0) {
-            return 0;
-        }
-
-        int n = prices.length;
-        int[][] dp = new int[n][3];
-
+        //dp[i][1] 不持有 正常
+        //dp[i][2] 不持有 冷冻期
+        // 持有= 最大值（昨天不持有  今天买入，维持昨天持有）
+        //dp[i][0]=Math.max(dp[i-1][1]-prices[i],dp[i-1][0]);
+        // 不持有= 最大值（昨天冷冻期过度，维持昨天不持有）
+        //dp[i][1]=Math.max(dp[i-1][2],dp[i-1][1])
+        // 冷冻期=  昨天卖掉
+        //dp[i][2]= dp[i-1][0]+prices[i];
+        int len = prices.length;
+        int[][] dp = new int[len][3];
 
         dp[0][0] = -prices[0];
-
-        for (int i = 1; i < n; i++) {
-            // 持有
-            dp[i][0] = Math.max(dp[i - 1][2] - prices[i], dp[i - 1][0]);
-            // 不持有 冷冻
-            dp[i][1] = dp[i - 1][0] + prices[i];
-            // 不持有 不冷冻
-            dp[i][2] = Math.max(dp[i - 1][1], dp[i - 1][2]);
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][1] - prices[i], dp[i - 1][0]);
+            dp[i][1] = Math.max(dp[i - 1][2], dp[i - 1][1]);
+            dp[i][2] = dp[i - 1][0] + prices[i];
         }
-        return Math.max(dp[n - 1][1], dp[n - 1][2]);
+        return Math.max(dp[len - 1][1], dp[len - 1][2]);
     }
 }
