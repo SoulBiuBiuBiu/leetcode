021630aebbd3a56fb53699a249e9bb349a23bc11
Barry <1172807506@qq.com>
2022-04-07 14:32:01 +0800

    'Hot100 P437-2'

--- src/main/java/org/barry/algorithm/hot100/P437.java
+++ src/main/java/org/barry/algorithm/hot100/P437.java
@@ -13,26 +13,30 @@ import java.util.Map;
  * @since 2022/3/14 14:21
  */
 public class P437 {
-    Map<Integer, Integer> map = new HashMap<>();
+
 
     public int pathSum(TreeNode root, int targetSum) {
-        map.put(0, 1);
-        return dfs(root, targetSum, 0);
+        Map<Integer, Integer> prefixSumCount = new HashMap<>();
+        prefixSumCount.put(0, 1);
+        return reursionPathSum(root, prefixSumCount, targetSum, 0);
     }
 
-    private int dfs(TreeNode root, int targetSum, int curSum) {
-        if (root == null) {
+    private int reursionPathSum(TreeNode node, Map<Integer, Integer> prefixSumCount, int targetSum, int curSum) {
+        if (node == null) {
             return 0;
         }
         int res = 0;
-        curSum += root.val;
+        curSum += node.val;
 
-        res = map.getOrDefault(curSum - targetSum, 0);
-        map.put(curSum, map.getOrDefault(curSum, 0) + 1);
-        res += dfs(root.left, targetSum, curSum);
-        res += dfs(root.right, targetSum, curSum);
-        map.put(curSum, map.getOrDefault(curSum, 0) - 1);
+        res += prefixSumCount.getOrDefault(curSum - targetSum, 0);
 
+        prefixSumCount.put(curSum, prefixSumCount.getOrDefault(curSum, 0) + 1);
+
+        res += reursionPathSum(node.left, prefixSumCount, targetSum, curSum);
+        res += reursionPathSum(node.right, prefixSumCount, targetSum, curSum);
+
+        prefixSumCount.put(curSum, prefixSumCount.getOrDefault(curSum, 0) - 1);
         return res;
     }
+
 }
