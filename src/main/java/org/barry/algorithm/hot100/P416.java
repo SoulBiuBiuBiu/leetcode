0196de886567ef3d36e38e0bacbabb522d55ede0
Barry <1172807506@qq.com>
2022-04-07 12:20:20 +0800

    'Hot100 P416-2'

--- src/main/java/org/barry/algorithm/hot100/P416.java
+++ src/main/java/org/barry/algorithm/hot100/P416.java
@@ -1,7 +1,5 @@
 package org.barry.algorithm.hot100;
 
-import java.util.Arrays;
-
 /**
  * 〈一句话功能简述〉
  * 〈功能详细描述〉
@@ -10,50 +8,45 @@ import java.util.Arrays;
  * @since 2022/3/11 17:23
  */
 public class P416 {
+    public static void main(String[] args) {
+        new P416().canPartition(new int[]{1, 5, 10, 6});
+    }
+
     public boolean canPartition(int[] nums) {
-        //dp[i][j]  nums[..i]中是否可以组成 j
-        //dp[i][j]= dp[i-1][j-nums[k]]
 
-
-        int n = nums.length;
-
-        if (n < 2) {
+        int length = nums.length;
+        if (length < 2) {
             return false;
         }
 
-        int sum = 0, maxNum = 0;
+        int sum = 0;
         for (int num : nums) {
             sum += num;
-            maxNum = Math.max(maxNum, num);
         }
-
         if (sum % 2 == 1) {
             return false;
         }
 
+
         int target = sum / 2;
-
-        if (maxNum > target) {
-            return false;
+        //dp[i][j] nums[..i]能否装满j
+        boolean[][] dp = new boolean[length][target + 1];
+        if (nums[0] <= target) {
+            dp[0][nums[0]] = true;
         }
+        for (int i = 1; i < length; i++) {
+            for (int j = 0; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
 
-        boolean[][] dp = new boolean[n][target + 1];
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
-        }
-        dp[0][nums[0]] = true;
-
-        for (int i = 1; i < n; i++) {
-            int num = nums[i];
-            for (int j = 1; j <= target; j++) {
-                if (j >= num) {
-                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
+                if (nums[i] == j) {
+                    dp[i][j] = true;
+                    continue;
+                }
+                if (nums[i] < j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
                 }
             }
         }
-
-        return dp[n - 1][target];
+        return dp[length - 1][target];
     }
 }
