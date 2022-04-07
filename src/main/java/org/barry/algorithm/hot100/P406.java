9ae1d70376fd980d8178f6fb5805ab86950913ea
Barry <1172807506@qq.com>
2022-04-07 11:41:33 +0800

    'Hot100 P406-2'

--- src/main/java/org/barry/algorithm/hot100/P406.java
+++ src/main/java/org/barry/algorithm/hot100/P406.java
@@ -3,7 +3,6 @@ package org.barry.algorithm.hot100;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 /**
  * 〈一句话功能简述〉
@@ -13,19 +12,24 @@ import java.util.List;
  * @since 2022/3/11 16:47
  */
 public class P406 {
+    public static void main(String[] args) {
+        new P406().reconstructQueue(new int[][]{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}
+        });
+    }
+
     public int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                if (o1[0] != o2[0]) {
-                    return o2[0] - o1[0];
-                } else {
+                if (o1[0] == o2[0]) {
                     return o1[1] - o2[1];
+                } else {
+                    return o2[0] - o1[0];
                 }
             }
         });
 
-        List<int[]> ans = new ArrayList<>();
+        ArrayList<int[]> ans = new ArrayList<>();
         for (int[] person : people) {
             ans.add(person[1], person);
         }
