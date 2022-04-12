541e767e25791ef90fa3948e44626514bf6173a2
Barry <1172807506@qq.com>
2022-04-12 10:32:21 +0800

    'Hot100 P739-2'

--- src/main/java/org/barry/algorithm/hot100/P739.java
+++ src/main/java/org/barry/algorithm/hot100/P739.java
@@ -1,6 +1,7 @@
 package org.barry.algorithm.hot100;
 
-import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  * 〈一句话功能简述〉
@@ -10,16 +11,22 @@ import java.util.Stack;
  * @since 2022/3/18 16:31
  */
 public class P739 {
+    public static void main(String[] args) {
+        new P739().dailyTemperatures(new int[]{
+                73, 74, 75, 71, 69, 72, 76, 73});
+    }
+
     public int[] dailyTemperatures(int[] temperatures) {
-        int n = temperatures.length;
-        int[] res = new int[temperatures.length];
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < temperatures.length; i++) {
-            while (!st.isEmpty() && temperatures[i] > temperatures[st.peek()]) {
-                int t = st.pop();
-                res[t] = i - t;
+        int length = temperatures.length;
+        int[] res = new int[length];
+        //当前单调递减
+        Deque<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < length; i++) {
+            while (!queue.isEmpty() && temperatures[queue.getLast()] < temperatures[i]) {
+                Integer prevIndex = queue.removeLast();
+                res[prevIndex] = i - prevIndex;
             }
-            st.push(i);
+            queue.addLast(i);
         }
         return res;
     }
