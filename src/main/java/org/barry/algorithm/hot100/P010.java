cbbae0d0e6da02855dec1a581b6bee07e10272c8
Barry <1172807506@qq.com>
2022-04-22 15:08:37 +0800

    'Hot100 P010'

---
+++ src/main/java/org/barry/algorithm/hot100/P010.java
@@ -0,0 +1,58 @@
+package org.barry.algorithm.hot100;
+
+/**
+ * 〈一句话功能简述〉
+ * 〈功能详细描述〉
+ *
+ * @author Barry
+ * @since 2022/4/22 11:54
+ */
+public class P010 {
+    public static void main(String[] args) {
+        new P010().isMatch("aab",
+                "c*a*b");
+    }
+
+    public boolean isMatch(String s, String p) {
+        //dp[i][j] s 的前 i 个是否能被 p 的前 j 个匹配
+        int n = s.length();
+        int m = p.length();
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+
+        // s为空，p不为空，由于*可以匹配0个字符，所以有可能为true
+        for (int j = 1; j <= p.length(); j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j] || dp[i][j - 1];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                }
+            }
+            print(dp);
+        }
+        return dp[n][m];
+    }
+
+    private void print(boolean[][] dp) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
