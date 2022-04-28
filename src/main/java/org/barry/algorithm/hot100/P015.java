04fb963b8879c04c1bcb0f6668afd446245ea263
Barry <1172807506@qq.com>
2022-04-28 15:23:19 +0800

    'hot100 P015-3'

--- src/main/java/org/barry/algorithm/hot100/P015.java
+++ src/main/java/org/barry/algorithm/hot100/P015.java
@@ -12,30 +12,31 @@ import java.util.List;
  * @since 2022/2/9 15:50
  */
 public class P015 {
+    public static void main(String[] args) {
+        new P015().threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+    }
+
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        int length = nums.length;
-        if (length <= 2) {
-            return ans;
-        }
-
-        for (int i = 0; i < length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) continue;
             int target = -nums[i];
-            int left = i + 1;
-            int right = length - 1;
+            //i+1 去重！！！
+            int left = i + 1, right = n - 1;
             while (left < right) {
-                if (nums[left] + nums[right] == target) {
+                int tmp = nums[left] + nums[right];
+                if (target == tmp) {
                     ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++; // 去重
+                    while (left < right && nums[right] == nums[right - 1]) right--; // 去重
                     left++;
                     right--;
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else if (nums[left] + nums[right] < target) {
-                    left++;
+                } else if (tmp > target) {
+                    right--;
                 } else {
-                    right--;
+                    left++;
                 }
             }
         }
