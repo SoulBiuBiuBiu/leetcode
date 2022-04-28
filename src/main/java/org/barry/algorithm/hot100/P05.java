c19ed6b6b76ac5e85e3613b601f71cf612a206db
Barry <1172807506@qq.com>
2022-04-28 11:50:48 +0800

    'hot100 P005-3'

--- src/main/java/org/barry/algorithm/hot100/P05.java
+++ src/main/java/org/barry/algorithm/hot100/P05.java
@@ -8,79 +8,24 @@ package org.barry.algorithm.hot100;
  * @since 2022/2/9 15:02
  */
 public class P05 {
-    /**
-     * 中心扩展解法
-     *
-     * @param s
-     */
-    public String longestPalindrome1(String s) {
-        int len = s.length();
-        if (len < 2) {
-            return s;
-        }
-        int start = 0, end = 0;
-        for (int i = 0; i < len - 1; i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int length = Math.max(len1, len2);
-            if (length > end - start) {
-                start = i - (length - 1) / 2;
-                end = i + length / 2;
-            }
-        }
-        return s.substring(start, end + 1);
-    }
-
-    public int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            right++;
-            left--;
-        }
-        return right - left - 1;
-    }
-
-    /**
-     * 动态规划
-     *
-     * @param s
-     */
     public String longestPalindrome(String s) {
-        //dp[i][j]  子串s[i..j] 是否为回文
-        //状态转移  dp[i][j]=  dp[i+1][j-1] && s.charAt(i)==s.charAt(j)
-        //边界条件 要构成区间  j-1 -(i+1) +1 <2    =>   j-i<3;
-        //初始化dp[i][i]=true;
-
-        int len = s.length();
-        if (len < 2) {
-            return s;
-        }
-
-        int maxLen = 1;
-        int begin = 0;
-
-        boolean[][] dp = new boolean[len][len];
-        for (int i = 0; i < len; i++) {
-            dp[i][i] = true;
-        }
-
-        for (int j = 1; j < len; j++) {
-            for (int i = 0; i < j; i++) {
-                if (s.charAt(i) != s.charAt(j)) {
-                    dp[i][j] = false;
-                } else {
-                    if (j - i < 3) {
-                        dp[i][j] = true;
-                    } else {
-                        dp[i][j] = dp[i + 1][j - 1];
-                    }
-                }
-
-                if (dp[i][j] && j - i + 1 > maxLen) {
-                    maxLen = j - i + 1;
-                    begin = i;
-                }
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            String s1 = palindrome(i, i, s);
+            String s2 = palindrome(i, i + 1, s);
+            if (Math.max(s1.length(), s2.length()) > res.length()) {
+                res = s1.length() > s2.length() ? s1 : s2;
             }
         }
-        return s.substring(begin, begin + maxLen);
+        return res;
     }
+
+    private String palindrome(int left, int right, String s) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+
 }
